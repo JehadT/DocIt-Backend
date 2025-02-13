74297d4b727c6d21f4dea679ed3e8a13e68316f5
JehadT <jjehad.tq1@gmail.com>
2025-02-13 12:29:56 +0300

    Added security packages

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+
 const express = require("express");
 const app = express();
 
-
-// Middleware
-app.use(express.json());
-
+// frontend
+app.use(express.static("dist"))
 
 // Database
 const connectDB = require("./db/connect");
@@ -21,21 +26,33 @@ const userInfoRouter = require("./routes/userInfo");
 const formRouter = require("./routes/forms");
 const fileRouter = require("./routes/files");
 
+// error handler
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy", 1);
+// use packages
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
 // routes
 app.use("/api/auth", authRouter);
 app.use("/api", authenticateUser, userInfoRouter);
 app.use("/api", authenticateUser, formRouter);
 app.use("/api", authenticateUser, fileRouter);
 
-
-// error handler
-const errorHandlerMiddleware = require("./middleware/error-handler");
-
-app.get("/", (req, res) => {
-  res.status(200).send("DocIt is running!");
-});
-
 app.use(errorHandlerMiddleware);
+
 
 const port = process.env.PORT || 3000;
 
